b18d3f0a75955fc4ab5a1f074f5d2d7a51d313c4
Imogen Taylor <it15562@bristol.ac.uk>
2018-10-11 16:10:14 +0100

    Completed up to q4 of numpy section

--- npysection.py
+++ npysection.py
@@ -12,3 +12,16 @@ a = np.array([1, 2, 3])
 a = [1,2,3]
 print(a+a)
 print(a*3)
+
+e = np.array([[1,2],[3,4]])
+c = np.array([[1,2,3],[0,0,1],[2,0,0]])
+d = np.array([[2,0,0],[0,2,0],[0,0,2]])
+
+f = np.array([10,20])
+def syslin(M,b):
+    invM = np.linalg.inv(M)
+    print(invM @ b)
+    
+    
+
+syslin(e,f)
