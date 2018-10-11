5892070da14e4d86c15e9905b81baae605f86104
Imogen Taylor <it15562@bristol.ac.uk>
2018-10-11 15:36:13 +0100

    Question 1 of numpy section complete

---
+++ npysection.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+""""Numpy section"""
+import numpy as np
+import scipy
+
+a = np.array([1, 2, 3])
+a = [1,2,3]
+print(a+a)
+print(a*3)
